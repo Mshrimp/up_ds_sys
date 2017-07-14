31d2e643ff5992fad75fa830097b995c052daa94
Mshrimp <chiyuan.ma@outlook.com>
2017-07-14 19:06:40 +0800

    sys 1st 1.c 2.c

--- sys/1st/1.c
+++ sys/1st/1.c
@@ -6,17 +6,20 @@ BUFSIZ作用:
 用户强制fflush；才进行写文件动作。BUFSIZ为系统默认的缓冲区大小。
 
 BUFSIZ使用:
+	默认BUFSIZ = 8192字节
 	#include <stdio.h> (引入头文件即可)
 	setbuf(stdout, buf); (修改默认缓冲大小)
-	默认BUFSIZ = 8192字节
+	setbuf()函数的第二个参数取值可以为null，此时标准输出不需要进行
+缓冲，程序运行速度变慢，系统负担变大。
 *****************************************************************/
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+//#include <string.h>
+//#include <stdlib.h>
 
 int main(void)
 {
 	//默认缓冲区大小
 	printf("BUFSIZ = %d\n", BUFSIZ);
+
 	return 0;
 }
--- sys/1st/2.c
+++ sys/1st/2.c
@@ -5,6 +5,9 @@ fread fwrite函数的使用
 
 	size: 每次传输的字节数
 	nmemb: 传输的数据个数
+
+	read和write过程中，数据个数达到nmemb或者到文件结尾或者出错才停止。
+	Return: 运行成功时返回值和nmemb相等。
 ************************************************************************/
 #include <stdio.h>
 #include <string.h>
@@ -15,8 +18,8 @@ int main(int argc, char *argv[])
 {
 	FILE *fpr = NULL, *fpw = NULL;
 	char buf[1024] = {};
-	//mode 1024 size 1
-	//mode 1 size 1024
+	//1. mode 1024 size 1
+	//2. mode 1 size 1024
 	int ret;
 
 	fpr = fopen(argv[1], "r");
@@ -32,23 +35,28 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-#if 0
+#if 1
 	// 每次读取sizeof(buf)并发送，一次传输
 	// fread 读取到字符串结尾时返回0
-	while((ret = fread(buf, sizeof(buf), 1, fpr)) == 1)
+	// 每次发送sizeof(buf)字节，发送1次，fread返回1
+	while((ret = fread(buf, sizeof(buf), 1, fpr)) > 0)
 	{
-		printf("ret = %d\n", ret);
-		fwrite(buf, sizeof(buf), 1, fpw);
+		printf("fread: ret = %d\n", ret);
+		ret = fwrite(buf, sizeof(buf), 1, fpw);
+		//printf("fwrite: ret = %d\n", ret);
 		memset(buf, 0, sizeof(buf));
 	}
 
+	// fread读取到的字节数小于size时，返回0，通过此处将数据fwrite
 	fwrite(buf, strlen(buf), 1, fpw);
 	/*printf("buf = %s\n", buf);*/
 #else
-	// 每次发送1字节，发送sizeof(buf)次
+	// 每次发送1字节，发送sizeof(buf)次，fread返回sizeof(buf)
 	while((ret = fread(buf, 1, sizeof(buf), fpr)) > 0)
 	{
-		fwrite(buf, 1, ret, fpw);
+		printf("fread: ret = %d\n", ret);
+		ret = fwrite(buf, 1, ret, fpw);
+		printf("fwrite: ret = %d\n", ret);
 	}
 #endif
 	fclose(fpr);
